a33b5ea49cc76bd0c6d469caf3a8dcb5e46a29d4
khayan <khayan.malantrucco@gmail.com>
2024-04-02 17:38:47 -0300

    Teste 3 com hover

--- teste3.js
+++ teste3.js
@@ -1,5 +1,5 @@
 /**
-    1. Com JS criar um círculo verde com 50px raio
+    1. Com JS criar um círculo verde com 500px raio
  */
 
 const circle = document.createElement('div');
@@ -7,6 +7,9 @@ const circle = document.createElement('div');
 circle.style.width = '500px';
 circle.style.height = '500px';
 circle.style.borderRadius = '50%';
-circle.style.backgroundColor = 'green';
+circle.style.backgroundColor = 'gray';
 
 document.body.appendChild(circle);
+
+circle.addEventListener('mouseenter', () => circle.style.backgroundColor = 'red');
+circle.addEventListener('mouseleave', () => circle.style.backgroundColor = 'green');
