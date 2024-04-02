3e7e44b59c66d506c580ea5df5a255f0a1e5f1d3
khayan <khayan.malantrucco@gmail.com>
2024-04-02 15:26:38 -0300

    Desafio 2

---
+++ teste2.js
@@ -0,0 +1,6 @@
+/**
+    1. Retornar a 7ª palavra de uma string de 10 palavras
+ */
+
+const stringDePalavras = "um dois três quatro cinco seis sete oito nove dez";
+console.log(stringDePalavras.split(" ")[6]);
